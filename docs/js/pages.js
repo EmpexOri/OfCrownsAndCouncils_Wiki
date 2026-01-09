dcf3ecb29640dc0e7ce3b578a5d4e3f77d71697a
Oliver Rayward <127711084+EmpexOri@users.noreply.github.com>
2026-01-09 18:55:02 +1100

    Update pages.js

--- docs/js/pages.js
+++ docs/js/pages.js
@@ -1,12 +1,8 @@
 var PAGES = [
-  {
-    title: "Home",
-    url: "/OfCrownsAndCouncils_Wiki/index.html"
-  },
-  {
-    title: "Lore",
-    url: "/OfCrownsAndCouncils_Wiki/pages/lore.html"
-  }
+  { title: "Home", url: "/OfCrownsAndCouncils_Wiki/index.html" },
+  { title: "Lore", url: "/OfCrownsAndCouncils_Wiki/pages/lore.html" },
+  { title: "Mechanics", url: "/OfCrownsAndCouncils_Wiki/pages/mechanics.html" }
 ];
+
 
 console.log("pages.js loaded");
